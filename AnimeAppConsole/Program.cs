5799712a466e7695f942a1443f5f67e4511a7ec5
akincengiz <akincengiz@outlook.com.tr>
2024-05-08 16:15:26 +0300

    Add project files.

---
+++ AnimeAppConsole/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AnimeAppConsole;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        using (var context = new AnimeAppDbContext())
+        {
+            var animeDetails = context.AnimeDetails.FromSqlRaw("select a.Id as Anime_Id, a.Name as Name, g.Type as GenreName from Animes as a join Genres as g on a.GenreId=g.Id").ToList();
+            foreach (var item in animeDetails)
+            {
+                Console.WriteLine($"Id : {item.Anime_Id} - Anime Name : {item.Name} - Genre : {item.GenreName}"); 
+            }
+            
+        }
+    }
+}
